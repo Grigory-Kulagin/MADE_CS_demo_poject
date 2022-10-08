5cfec0e2f5694881941bc91eb4091567628d3295
Grigory Kulagin <grigorykulagin4276@gmail.com>
2022-10-08 13:25:21 +0300

    Add divide function

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def sum_nums(num1, num2):
 def subtract(num1, num2):
     print(f"{num1} - {num1} = {num1 - num2}")
 
+def divide_nums(num1, num2):
+    print(f"{num1} / {num1} = {num1 / num2}")
+
 
 print('Hello word!')
 sum_nums(1, 2)
